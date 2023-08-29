008e713d60607449b0adb38ebf341d70924d606f
imgonnabe <qorlwn456123@gmail.com>
2023-08-29 17:09:19 +0900

    타임리프

---
+++ src/test/java/com/qorlwn/web/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qorlwn.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
